d5758613fc1d53b9bd1959cf844bed8214d3d8eb
eqqmayo <eqqmayo@gmail.com>
2024-09-11 23:58:35 +0900

    feat: 게시일 확인 rss 방식으로 변경

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,21 @@
 import 'dart:io';
 import 'package:bobjobot/obob.dart';
 import 'package:cron/cron.dart';
-// import 'package:dotenv/dotenv.dart';
+import 'package:dotenv/dotenv.dart';
 
 void main() async {
-  // final env = DotEnv(includePlatformEnvironment: true)..load();
-  // final token = env['DISCORD_BOT_TOKEN'];
-
-  final token = Platform.environment['DISCORD_BOT_TOKEN'];
+  final env = DotEnv(includePlatformEnvironment: true)..load();
+  final token = env['DISCORD_BOT_TOKEN'];
+  // final token = Platform.environment['DISCORD_BOT_TOKEN'];
 
   if (token == null) {
     throw Exception('토큰 찾을 수 없음');
   }
 
   final cron = Cron();
-  final obob = Obob(token: token, channelIds: [1230352884462260286]);
+  // final obob = Obob(token: token, channelIds: [1230352884462260286]);
+  final obob = Obob(token: token, channelIds: [1283369469275668504]);
 
   cron.schedule(
-      Schedule.parse('*/5 11-12 * * 3,4,5'), () => obob.activateBot());
+      Schedule.parse('*/1 19-21 * * 3,4,5'), () => obob.activateBot());
 }
--- lib/obob.dart
+++ lib/obob.dart
@@ -4,9 +4,12 @@ import 'package:html/dom.dart';
 import 'package:nyxx/nyxx.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/parser.dart' as parser;
+import 'package:xml/xml.dart';
+import 'package:intl/intl.dart';
 
 class Obob {
   static const String blogUrl = 'https://blog.naver.com/skfoodcompany';
+  static const String rssUrl = 'https://rss.blog.naver.com/skfoodcompany.xml';
   static const int maxImages = 10;
 
   final String _token;
@@ -35,111 +38,123 @@ class Obob {
 
   Future<void> activateBot() async {
     await _initializationDone;
+    if (await _isPostFromToday() && _messageSentTracker[_todayKey] == false) {
+      await _processAndPostLunchMenu();
+    }
+  }
 
+  Future<bool> _isPostFromToday() async {
+    try {
+      final response = await http.get(Uri.parse(rssUrl));
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load rss: ${response.statusCode}');
+      }
+
+      final document = XmlDocument.parse(response.body);
+      final items = document.findAllElements('item');
+      if (items.isEmpty) throw Exception('No posts found');
+
+      return _isToday(items.first);
+    } catch (e) {
+      stderr.writeln('Error checking latest post: $e');
+      return false;
+    }
+  }
+
+  bool _isToday(XmlElement post) {
+    final pubDateString = post.findElements('pubDate').single.innerText;
+    final pubDate =
+        DateFormat("EEE, dd MMM yyyy HH:mm:ss Z", 'en_US').parse(pubDateString);
+    final today = DateTime.now();
+
+    return pubDate.year == today.year &&
+        pubDate.month == today.month &&
+        pubDate.day == today.day;
+  }
+
+  Future<void> _processAndPostLunchMenu() async {
     try {
       final iframeElement = await _getElements(url: blogUrl, tag: 'iframe');
+      if (iframeElement == null) return;
 
-      if (iframeElement != null) {
-        final src = iframeElement[0].attributes['src'];
-        final fullSrc = Uri.parse(blogUrl).resolve(src!).toString();
+      final src = iframeElement[0].attributes['src'];
+      final fullSrc = Uri.parse(blogUrl).resolve(src!).toString();
+      final imageElements =
+          await _getElements(url: fullSrc, tag: 'img', isAll: true);
+      if (imageElements == null) return;
 
-        final imageElements =
-            await _getElements(url: fullSrc, tag: 'img', isAll: true);
-        final dateElement =
-            await _getElements(url: fullSrc, tag: 'h3.se_textarea');
-
-        if (imageElements != null && dateElement != null) {
-          final dateText = dateElement[0].text.trim();
-          final images = await _getLunchImages(dateText, imageElements);
-
-          if (images != null && images.isNotEmpty) {
-            await _postImageToDiscord(images);
-          }
-        }
+      final images = await _getLunchImages(imageElements);
+      if (images != null && images.isNotEmpty) {
+        await _postImageToDiscord(images);
       }
     } catch (e) {
-      stderr.writeln('Failed to activateBot: $e');
+      stderr.writeln('Failed to process and post lunch menu: $e');
     }
+  }
+
+  Future<List<Element>?> _getElements(
+      {required String url, required String tag, bool isAll = false}) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load URL: ${response.statusCode}');
+      }
+
+      final document = parser.parse(response.body);
+      if (isAll) {
+        return document.querySelectorAll(tag);
+      }
+
+      final element = document.querySelector(tag);
+      if (element == null) {
+        throw Exception('Cannot find $tag');
+      }
+      return [element];
+    } catch (e) {
+      stderr.writeln('Error getting elements: $e');
+      return null;
+    }
+  }
+
+  Future<List<Uint8List>?> _getLunchImages(List<Element> elements) async {
+    final imageUrls = elements
+        .map((img) => img.attributes['data-lazy-src'])
+        .whereType<String>()
+        .toList()
+        .take(maxImages);
+
+    final List<Uint8List> images = [];
+
+    for (String imageUrl in imageUrls) {
+      final response = await http.get(Uri.parse(imageUrl));
+      if (response.statusCode == 200) {
+        images.add(response.bodyBytes);
+      } else {
+        stderr.writeln('Failed to fetch image: $imageUrl');
+      }
+    }
+    return images;
   }
 
   Future<void> _postImageToDiscord(List<Uint8List> images) async {
     try {
-      final channels = await Future.wait(_channelIds
-          .map((id) async => await _bot.channels.fetch(Snowflake(id))));
-
-      for (var channel in channels) {
-        if (channel is GuildTextChannel) {
-          await channel.sendMessage(MessageBuilder()
-            ..attachments = images
-                .map((image) => AttachmentBuilder(
-                    data: image,
-                    fileName: 'lunchmenu_${images.indexOf(image)}.jpg'))
-                .toList());
-
-          _messageSentTracker[_todayKey] = true;
-        } else {
-          stderr.writeln('텍스트 채널이 아닙니다.');
-        }
-      }
+      final channels = await Future.wait(
+          _channelIds.map((id) => _bot.channels.fetch(Snowflake(id))));
+      final textChannels = channels.whereType<GuildTextChannel>().toList();
+      await Future.wait(
+          textChannels.map((channel) => _sendImagesToChannel(channel, images)));
+      _messageSentTracker[_todayKey] = true;
     } catch (e) {
       stderr.writeln('이미지 전송 중 오류 발생: $e');
     }
   }
 
-  Future<List<Uint8List>?> _getLunchImages(
-      String dateText, List<Element> elements) async {
-    if (_isToday(dateText)) {
-      if (_messageSentTracker[_todayKey] != true) {
-        final imageUrls = elements
-            .map((img) => img.attributes['data-lazy-src'])
-            .whereType<String>()
-            .toList()
-            .take(maxImages);
-
-        final List<Uint8List> images = [];
-
-        for (String imageUrl in imageUrls) {
-          final response = await http.get(Uri.parse(imageUrl));
-          if (response.statusCode == 200) {
-            images.add(response.bodyBytes);
-          } else {
-            stderr.writeln('Failed to fetch image: $imageUrl');
-          }
-        }
-        return images;
-      } else {
-        stderr.writeln('오늘은 이미 알림을 완료하였습니다.');
-      }
-    }
-    return null;
-  }
-
-  bool _isToday(String dateText) {
-    DateTime today = DateTime.now();
-    RegExp regExp = RegExp(r'(\d+)월(\d+)일');
-    Match? match = regExp.firstMatch(dateText);
-
-    return match != null &&
-        int.parse(match[1]!) == today.month &&
-        int.parse(match[2]!) == today.day;
-  }
-
-  Future<List<Element>?> _getElements(
-      {required String url, required String tag, bool isAll = false}) async {
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode != 200) {
-      stderr.writeln('Failed to load url: ${response.statusCode}');
-      return null;
-    }
-    final document = parser.parse(response.body);
-    if (isAll) {
-      return document.querySelectorAll(tag);
-    }
-    final element = document.querySelector(tag);
-    if (element != null) {
-      return [element];
-    }
-    stderr.writeln('Cannot find $tag');
-    return null;
+  Future<void> _sendImagesToChannel(
+      GuildTextChannel channel, List<Uint8List> images) async {
+    await channel.sendMessage(MessageBuilder()
+      ..attachments = images.asMap().entries.map((entry) {
+        return AttachmentBuilder(
+            data: entry.value, fileName: 'lunchmenu_${entry.key}.jpg');
+      }).toList());
   }
 }
